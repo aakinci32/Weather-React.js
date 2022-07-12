fc8183a9f642097dc7428c02732a85bddfdd0bef
aakinci32 <102487553+aakinci32@users.noreply.github.com>
2022-07-11 20:29:25 -0400

    Update App.js

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 function App() {
-  const [mealData, setMealData] = useState(null);
-  const [calories, setCalories] = useState(2000);
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
 
